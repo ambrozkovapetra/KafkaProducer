e24df6d6d81a26df8ef82078b7367cbb6bb0a3f6
ambrozkovapetra <ambrozkovapetra@gmail.com>
2015-09-01 15:25:30 +0200

    Kafka producer

--- src/main/java/cz/muni/fi/kafkaproducer/PropertiesParser.java
+++ src/main/java/cz/muni/fi/kafkaproducer/PropertiesParser.java
@@ -4,16 +4,13 @@ import java.io.*;
 import java.util.Properties;
 import java.io.InputStream;
 
-/**
- *
- * @author Petra
- */
+
 public class PropertiesParser{
 
     private static final String propertiesFile = "src/main/resources/kafka.properties";
 
     /**
-     * Parsers esper.properties file
+     * Parsers kafka.properties file
      *
      */
     public static Properties getProperties() {
